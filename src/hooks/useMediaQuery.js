2b4468b489f79072ed7cabb496088a129b726d81
Smith Ringtho <sringtho@gmail.com>
2023-03-30 05:53:42 +0000

    Added Tailwind library

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react"
+
+
+function useMediaQuery(query){
+    const [matches, setMatches] = useState(false)
+
+    useEffect(() => {
+        const media = window.matchMedia(query)
+        if(media.matches !== matches){
+            setMatches(media.matches)
+        }
+
+        const listener = () => setMatches(media.matches)
+        window.addEventListener("resize", listener)
+        
+        return () => window.removeEventListener("resize", listener)
+
+    }, [query, matches])
+
+    return matches
+}
+
+export default useMediaQuery
