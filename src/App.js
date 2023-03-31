3ee0aa6d94769cb1de4adb46a8225b2e0cfa68e6
Smith Ringtho <sringtho@gmail.com>
2023-03-31 11:35:47 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function App() {
          </motion.div>
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto">
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
@@ -84,7 +84,7 @@ function App() {
          </motion.div>
       </div>
       <LineGradient />
-      <div className="w-5/6 mx-auto md:h-full">
+      <div className="w-5/6 mx-auto">
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
